e001404ceed1ee6f594e5aff8a6a7451f35a6f07
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-12-07 08:38:02 -0600

    0 tiles spawn

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.8 ? Math.random() < 0.9 ? 2 : 4 : Math.random() < 0.75 ? 8 : 0;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -140,8 +140,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 4194304 tile
+          if (merged.value === 4194304) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
